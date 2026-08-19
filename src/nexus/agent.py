4079e6a1db36e229a02c60dfe2a16a4fb3383fcd
S-Astralcoder <suraj.astralcode.ai@gmail.com>
2026-08-19 13:57:16 +0530

    feat: Enhance Agent and ToolBook classes with tool execution capabilities and improved response handling

--- src/nexus/agent.py
+++ src/nexus/agent.py
@@ -1,31 +1,70 @@
 # External
+import json
 from litellm import completion #pyright: ignore
 from dataclasses import asdict
+from rich.console import Console
+
 # Internal
 from nexus.config import AgentConfig
 from nexus.memory import AgentMemory
 from nexus.tool import ToolBook
 
 
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="pydantic")
+
+
 class Agent:
-    def __init__(self, agent_config : AgentConfig, agent_memory : AgentMemory, tool_book : ToolBook | None = None) -> None:
+    def __init__(self, agent_config : AgentConfig, agent_memory : AgentMemory, tool_book : ToolBook | None = None, show_tool_calls : bool = True) -> None:
         self.agent_config = agent_config
         self.agent_memory = agent_memory
+        self.tool_book = tool_book
+        self.show_tool_calls = show_tool_calls
 
+        self.console = Console()
 
     def get_response(self, message : str):
         try:
-            response = completion(**asdict(self.agent_config), messages=self.agent_memory.get_all_memory())
+            tools : list[dict] | None =  None
+            if self.tool_book is not None:
+                tools = self.tool_book.get_tools_schema()                
+            response = completion(**asdict(self.agent_config), messages=self.agent_memory.get_all_memory(), tools=tools)
             return response
         except Exception as e:
             print(f"Error Occurred : {e}")
             raise SystemExit("Aboding Operation...")
+
+    def execute_tool(self, function_name : str, arguments : dict):
+        if self.tool_book is None:
+            raise Exception("Tool call has been called when no function exists")
+        tool_links = self.tool_book.get_tool_call_links()
+        return tool_links[function_name](**arguments)
+
+
+    def execute_tool_calls(self, tool_calls):
+        for tool_call in tool_calls:
+            tool_call_id = tool_call.id
+            function_name = tool_call.function.name
+            arguments = json.loads(tool_call.function.arguments)
+            if self.show_tool_calls:
+                self.console.print(f"[yellow]Tool {function_name} was called with arguments {json.dumps(arguments)}\n")
+            try:
+                result = self.execute_tool(function_name=function_name, arguments=arguments) #pyright: ignore
+            except Exception as e:
+                self.console.print(f"[red]Error Occurred while calling tool : {e}")
+                self.agent_memory.add_tool_call_result(tool_call_id=tool_call_id, result_content=f"{e}")
+                continue
+            if self.show_tool_calls:
+                self.console.print(f"[yellow]Tool {function_name} call resulted with response :\n{result}\n")
+            self.agent_memory.add_tool_call_result(tool_call_id=tool_call_id, result_content=result)
 
     def send_prompt(self, message : str, max_iteration : int = 10) -> str:
         self.agent_memory.add_memory(role="user", content=message)
         for _ in range(max_iteration):
             response = self.get_response(message=message)
             if response.choices[0].message.tool_calls: #pyright: ignore
-                pass
+                self.agent_memory.add_tool_call_memory(response.choices[0].message) #pyright: ignore
+                self.execute_tool_calls(tool_calls=response.choices[0].message.tool_calls) #pyright: ignore
+                continue
             return response.choices[0].message.content #pyright: ignore
         return "[ALERT] Max Iteration Reached"
--- src/nexus/tool.py
+++ src/nexus/tool.py
@@ -2,12 +2,61 @@ from typing import Any, Callable
 
 
 class ToolBook:
-    tool_book = {}
-    def __init__(self) -> None:
-        pass
+    _tool_book : list[dict] = []
+    _tool_links : dict[Any, Any] = {} 
+    def __init__(self, tags : list[str] | None = None) -> None:
+        self.tags = tags
 
 
-    def tool(self, name : str, description : str, tags : list[str]):
+    def get_tools_schema(self):
+        tool_list : list[dict] = []
+        for tool in self._tool_book:
+            if tool["tag"] == self.tags or tool["tag"] in self.tags:
+                tool_list.append(tool["function_info"])
+        return tool_list
+
+    @classmethod
+    def get_tool_call_links(cls):
+        return cls._tool_links
+
+    def _type_to_str(self, parameter_type) -> str:
+        if parameter_type is str:
+            return "string"
+        if parameter_type is int:
+            return "integer"
+        if parameter_type is float:
+            return "float"
+        if parameter_type is bool:
+            return "boolean"
+        raise Exception(f"parameter type : {type} not supported")
+
+    def _get_formatted_function_parameter(self, function_parameter : dict):
+        parameter : dict[Any, Any] = {}
+        for parameter_name, parameter_type in function_parameter.items():
+            parameter.update({parameter_name : {"type" : self._type_to_str(parameter_type=parameter_type)}})
+        return parameter
+
+    def add_tool(self, name : str | None = None , description : str | None = None, tag : str | None = None):
         def decorator(func : Callable[..., Any]):
+            function_name = name or func.__name__
+            function_description = description or func.__doc__
+            function_parameters = func.__annotations__
+
+            self._tool_links.update({function_name : func})
+
+            self._tool_book.append({
+                "tag" : tag,
+                "function_info" : {
+                    "type" : "function",
+                    "function" : {
+                        "name" : function_name,
+                        "description" : function_description or "",
+                        "parameters" : {
+                            "type" : "object",
+                            "properties" : self._get_formatted_function_parameter(function_parameter=function_parameters)
+                        }
+                    }
+                }
+            })
             return func
         return decorator
